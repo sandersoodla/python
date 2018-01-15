bd10df4f53662a04986aa3cd14deee24ae8ff9fe
sandersoodla <34090029+sandersoodla@users.noreply.github.com>
2018-01-15 08:24:00 +0200

    Delete lubadus.py

--- lubadus.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-
-aken = tkinter.Tk()
-aken.title("Lubadus")
-aken.geometry("1200x800")
-
-lubadus = tkinter.Label(text="Hakkan õppima Javascripti, tulemusena saan koodist hästi aru ja oskan ka ise programme kirjutada.", font=("Helvetica", 16))
-lubadus.pack()
